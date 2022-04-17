759e85c5e3a2c4040bab2eebc4b1137f2f4fb72e
Luke Leigh <luke.leigh@gmail.com>
2022-04-18 00:23:24 +0100

    Create EnvPaths.ps1

---
+++ EnvPaths.ps1
@@ -0,0 +1,78 @@
+function Add-EnvPath {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $Path,
+
+        [ValidateSet('Machine', 'User', 'Session')]
+        [string] $Container = 'Session'
+    )
+
+    if ($Container -ne 'Session') {
+        $containerMapping = @{
+            Machine = [EnvironmentVariableTarget]::Machine
+            User = [EnvironmentVariableTarget]::User
+        }
+        $containerType = $containerMapping[$Container]
+
+        $persistedPaths = [Environment]::GetEnvironmentVariable('Path', $containerType) -split ';'
+        if ($persistedPaths -notcontains $Path) {
+            $persistedPaths = $persistedPaths + $Path | Where-Object { $_ }
+            [Environment]::SetEnvironmentVariable('Path', $persistedPaths -join ';', $containerType)
+        }
+    }
+
+    $envPaths = $env:Path -split ';'
+    if ($envPaths -notcontains $Path) {
+        $envPaths = $envPaths + $Path | Where-Object { $_ }
+        $env:Path = $envPaths -join ';'
+    }
+}
+
+function Remove-EnvPath {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $Path,
+
+        [ValidateSet('Machine', 'User', 'Session')]
+        [string] $Container = 'Session'
+    )
+
+    if ($Container -ne 'Session') {
+        $containerMapping = @{
+            Machine = [EnvironmentVariableTarget]::Machine
+            User = [EnvironmentVariableTarget]::User
+        }
+        $containerType = $containerMapping[$Container]
+
+        $persistedPaths = [Environment]::GetEnvironmentVariable('Path', $containerType) -split ';'
+        if ($persistedPaths -contains $Path) {
+            $persistedPaths = $persistedPaths | Where-Object { $_ -and $_ -ne $Path }
+            [Environment]::SetEnvironmentVariable('Path', $persistedPaths -join ';', $containerType)
+        }
+    }
+
+    $envPaths = $env:Path -split ';'
+    if ($envPaths -contains $Path) {
+        $envPaths = $envPaths | Where-Object { $_ -and $_ -ne $Path }
+        $env:Path = $envPaths -join ';'
+    }
+}
+
+function Get-EnvPath {
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateSet('Machine', 'User')]
+        [string] $Container
+    )
+
+    $containerMapping = @{
+        Machine = [EnvironmentVariableTarget]::Machine
+        User = [EnvironmentVariableTarget]::User
+    }
+    $containerType = $containerMapping[$Container]
+
+    [Environment]::GetEnvironmentVariable('Path', $containerType) -split ';' |
+        Where-Object { $_ }
+}
+
+# Export-ModuleMember -Function *
