3d95476acbfbdff5a75fee51c046d3ee229d5e99
Luke Leigh <luke.leigh@gmail.com>
2022-04-18 00:23:29 +0100

    Create Get-EnvPath.ps1

---
+++ PSIISCrypto/Private/Get-EnvPath.ps1
@@ -0,0 +1,16 @@
+function Get-EnvPath {
+    param(
+        [Parameter(Mandatory=$true)]
+        [ValidateSet('Machine', 'User')]
+        [string] $Container
+    )
+
+    $containerMapping = @{
+        Machine = [EnvironmentVariableTarget]::Machine
+        User = [EnvironmentVariableTarget]::User
+    }
+    $containerType = $containerMapping[$Container]
+
+    [Environment]::GetEnvironmentVariable('Path', $containerType) -split ';' |
+        Where-Object { $_ }
+}
