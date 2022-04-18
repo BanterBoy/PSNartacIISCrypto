c185143b4ed0c1a578f82e665cc9334f4e769b62
Luke Leigh <luke.leigh@gmail.com>
2022-04-18 01:42:56 +0100

    Update Update-IISCrypto.ps1

--- PSIISCrypto/Public/Update-IISCrypto.ps1
+++ PSIISCrypto/Public/Update-IISCrypto.ps1
@@ -1,28 +1,40 @@
 function Update-IISCrypto {
     $EnvPath = Get-EnvPath -Container Machine | Where-Object -FilterScript { $_ -Like '*IISCrypto*' }
-    Invoke-RestMethod -Uri "https://www.nartac.com/Downloads/IISCrypto/IISCryptoCli.exe" -OutFile "$env:TEMP\IISCryptoCli.exe"
-    Invoke-RestMethod -Uri "https://www.nartac.com/Downloads/IISCrypto/IISCrypto.exe" -OutFile "$env:TEMP\IISCrypto.exe"
-    $CLIFile = Get-Item -Path "$EnvPath\IISCryptoCli.exe"
-    $GUIFile = Get-Item -Path "$EnvPath\IISCrypto.exe"
-            
-    $TempCLI = Get-ChildItem -Path $env:TEMP\IISCryptoCli.exe
-    $TempGUI = Get-ChildItem -Path $env:TEMP\IISCrypto.exe
-        
-    if ($CLIFile.VersionInfo.FileVersion -eq $TempCLI.VersionInfo.FileVersion) {
-        Write-Output "Curent Version up-to-date - $($CLIFile.VersionInfo.FileVersion)"
+    if (!$EnvPath) {
+        Write-Output "IISCrypto not found"
+        return
     }
     else {
-        Copy-Item -Path $TempCLI -Destination $EnvPath
-        Write-Output "Updated to latest version - $($CLIFile.VersionInfo.FileVersion)"
-        Remove-Item -Path $TempCLI
-    }
+        $CLIFile = Get-Item -Path "$EnvPath\IISCryptoCli.exe"
+        $GUIFile = Get-Item -Path "$EnvPath\IISCrypto.exe"
 
-    if ($GUIFile.VersionInfo.FileVersion -eq $TempGUI.VersionInfo.FileVersion) {
-        Write-Output "Curent Version up-to-date - $($GUIFile.VersionInfo.FileVersion)"
-    }
-    else {
-        Copy-Item -Path $TempGUI -Destination $EnvPath
-        Write-Output "Updated to latest version - $($GUIFile.VersionInfo.FileVersion)"
-        Remove-Item -Path $TempGUI
+        Invoke-RestMethod -Uri "https://www.nartac.com/Downloads/IISCrypto/IISCryptoCli.exe" -OutFile "$env:TEMP\IISCryptoCli.exe"
+        Invoke-RestMethod -Uri "https://www.nartac.com/Downloads/IISCrypto/IISCrypto.exe" -OutFile "$env:TEMP\IISCrypto.exe"
+        
+        $TempCLI = Get-Item -Path $env:TEMP\IISCryptoCli.exe
+        $TempGUI = Get-Item -Path $env:TEMP\IISCrypto.exe
+
+
+        if (Test-Path -Path $CLIFile) {
+            if ($CLIFile.VersionInfo.FileVersion -eq $TempCLI.VersionInfo.FileVersion) {
+                Write-Output "IISCrypto CLI up-to-date - $($CLIFile.VersionInfo.FileVersion)"
+                Remove-Item -Path $TempCLI
+            }
+            else {
+                Copy-Item -Path $TempCLI -Destination $EnvPath
+                Write-Output "IISCrypto CLI updated to version - $($CLIFile.VersionInfo.FileVersion)"
+                Remove-Item -Path $TempCLI
+            }
+            if ($GUIFile.VersionInfo.FileVersion -eq $TempGUI.VersionInfo.FileVersion) {
+                Write-Output "IISCrypto GUI up-to-date - $($GUIFile.VersionInfo.FileVersion)"
+                Remove-Item -Path $TempGUI
+            }
+            else {
+                Copy-Item -Path $TempGUI -Destination $EnvPath
+                Write-Output "IISCrypto GUI updated to version - $($GUIFile.VersionInfo.FileVersion)"
+                Remove-Item -Path $TempGUI
+            }
+        }
     }
 }
+
