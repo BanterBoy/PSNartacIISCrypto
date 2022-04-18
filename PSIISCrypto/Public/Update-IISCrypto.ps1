e7695bf76f40a5796c2981c123704bc543b17bca
Luke Leigh <luke.leigh@gmail.com>
2022-04-18 01:02:48 +0100

    Uninstall-IISCrypto and Update-IISCrpto added

--- PSIISCrypto/Public/Update-IISCrypto.ps1
+++ PSIISCrypto/Public/Update-IISCrypto.ps1
@@ -1,15 +1,28 @@
-
 function Update-IISCrypto {
-    Invoke-RestMethod -Uri "https://www.nartac.com/Downloads/IISCrypto/IISCryptoCli.exe" -OutFile "$env:TEMP\IISCrypto.exe"
-    $File = Get-Item -Path "$env:TEMP\IISCrypto.exe"
-    $IISCrypto = Get-ChildItem -Path C:\CustomPath\IISCrypto\IISCryptoCli.exe
-    
-    if ($IISCrypto.VersionInfo.FileVersion -eq $File.VersionInfo.FileVersion) {
-        Write-Output "Curent Version up-to-date - $($File.VersionInfo.FileVersion)"
+    $EnvPath = Get-EnvPath -Container Machine | Where-Object -FilterScript { $_ -Like '*IISCrypto*' }
+    Invoke-RestMethod -Uri "https://www.nartac.com/Downloads/IISCrypto/IISCryptoCli.exe" -OutFile "$env:TEMP\IISCryptoCli.exe"
+    Invoke-RestMethod -Uri "https://www.nartac.com/Downloads/IISCrypto/IISCrypto.exe" -OutFile "$env:TEMP\IISCrypto.exe"
+    $CLIFile = Get-Item -Path "$EnvPath\IISCryptoCli.exe"
+    $GUIFile = Get-Item -Path "$EnvPath\IISCrypto.exe"
+            
+    $TempCLI = Get-ChildItem -Path $env:TEMP\IISCryptoCli.exe
+    $TempGUI = Get-ChildItem -Path $env:TEMP\IISCrypto.exe
+        
+    if ($CLIFile.VersionInfo.FileVersion -eq $TempCLI.VersionInfo.FileVersion) {
+        Write-Output "Curent Version up-to-date - $($CLIFile.VersionInfo.FileVersion)"
     }
     else {
-        Copy-Item -Path $File -Destination $IISCrypto
-        Write-Output "Updated to latest version - $($File.VersionInfo.FileVersion)"
+        Copy-Item -Path $TempCLI -Destination $EnvPath
+        Write-Output "Updated to latest version - $($CLIFile.VersionInfo.FileVersion)"
+        Remove-Item -Path $TempCLI
     }
-    Remove-Item -Path $File
+
+    if ($GUIFile.VersionInfo.FileVersion -eq $TempGUI.VersionInfo.FileVersion) {
+        Write-Output "Curent Version up-to-date - $($GUIFile.VersionInfo.FileVersion)"
+    }
+    else {
+        Copy-Item -Path $TempGUI -Destination $EnvPath
+        Write-Output "Updated to latest version - $($GUIFile.VersionInfo.FileVersion)"
+        Remove-Item -Path $TempGUI
+    }
 }
