a874ec3b936f2942cb9b654b9ab2e60576c6fd9a
Luke Leigh <luke.leigh@gmail.com>
2022-04-18 16:47:36 +0100

    Create Uninstall-IISCrypto.ps1

---
+++ PSIISCrypto/Public/Uninstall-IISCrypto.ps1
@@ -0,0 +1,12 @@
+function Uninstall-IISCrypto {
+    $EnvPath = Get-EnvPath -Container Machine | Where-Object -FilterScript { $_ -Like '*IISCrypto*' }
+    if ($null -eq $EnvPath) {
+        Write-Output "IISCrypto is not installed on this machine"
+    }
+    else {
+        $Path = ($EnvPath).Split('IISCrypto')
+        Remove-Item -Path $Path -Recurse -Force
+        Remove-EnvPath -Path $EnvPath -Container Machine
+        Write-Output "IISCrypto is uninstalled from this machine"
+    }
+}
