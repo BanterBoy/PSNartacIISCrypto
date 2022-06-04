756fed44e5ee0e510f8839995af38eb5199db5b8
Luke Leigh <luke.leigh@gmail.com>
2022-06-04 12:15:40 +0100

    Update Remove-EnvPath.ps1

--- PSIISCrypto/Public/Remove-EnvPath.ps1
+++ PSIISCrypto/Public/Remove-EnvPath.ps1
@@ -1,17 +1,16 @@
 function Remove-EnvPath {
     [CmdletBinding(DefaultParameterSetName = 'Default')]
     param(
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [string] $Path,
 
         [ValidateSet('Machine', 'User', 'Session')]
         [string] $Container = 'Session'
     )
-
     if ($Container -ne 'Session') {
         $containerMapping = @{
             Machine = [EnvironmentVariableTarget]::Machine
-            User = [EnvironmentVariableTarget]::User
+            User    = [EnvironmentVariableTarget]::User
         }
         $containerType = $containerMapping[$Container]
 
@@ -21,7 +20,6 @@ function Remove-EnvPath {
             [Environment]::SetEnvironmentVariable('Path', $persistedPaths -join ';', $containerType)
         }
     }
-
     $envPaths = $env:Path -split ';'
     if ($envPaths -contains $Path) {
         $envPaths = $envPaths | Where-Object { $_ -and $_ -ne $Path }
