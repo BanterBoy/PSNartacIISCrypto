d37a8a6cfa1431f3c741bd3cbf3ca5bb0104aedf
Luke Leigh <luke.leigh@gmail.com>
2022-06-04 12:15:46 +0100

    Update Update-IISCrypto.ps1

--- PSIISCrypto/Public/Update-IISCrypto.ps1
+++ PSIISCrypto/Public/Update-IISCrypto.ps1
@@ -1,7 +1,5 @@
 function Update-IISCrypto {
     [CmdletBinding(DefaultParameterSetName = 'Default')]
-
-    
     $EnvPath = Get-EnvPath -Container Machine | Where-Object -FilterScript { $_ -Like '*IISCrypto*' }
     if (!$EnvPath) {
         Write-Output "IISCrypto not found"
@@ -10,14 +8,10 @@ function Update-IISCrypto {
     else {
         $CLIFile = Get-Item -Path "$EnvPath\IISCryptoCli.exe"
         $GUIFile = Get-Item -Path "$EnvPath\IISCrypto.exe"
-
         Invoke-RestMethod -Uri "https://www.nartac.com/Downloads/IISCrypto/IISCryptoCli.exe" -OutFile "$env:TEMP\IISCryptoCli.exe"
         Invoke-RestMethod -Uri "https://www.nartac.com/Downloads/IISCrypto/IISCrypto.exe" -OutFile "$env:TEMP\IISCrypto.exe"
-        
         $TempCLI = Get-Item -Path $env:TEMP\IISCryptoCli.exe
         $TempGUI = Get-Item -Path $env:TEMP\IISCrypto.exe
-
-
         if (Test-Path -Path $CLIFile) {
             if ($CLIFile.VersionInfo.FileVersion -eq $TempCLI.VersionInfo.FileVersion) {
                 Write-Output "IISCrypto CLI up-to-date - $($CLIFile.VersionInfo.FileVersion)"
@@ -40,4 +34,3 @@ function Update-IISCrypto {
         }
     }
 }
-
