15936745056b5b0203d699f4379b9affa57b4491
Luke Leigh <luke.leigh@gmail.com>
2024-07-19 03:58:16 +0100

    Update Set-IISCrypto.ps1

--- PSIISCrypto/Public/Set-IISCrypto.ps1
+++ PSIISCrypto/Public/Set-IISCrypto.ps1
@@ -1,218 +1,110 @@
 function Set-IISCrypto {
-
 	<#
-	.SYNOPSIS
-		Set-IISCrypto - Configure your server to use a strong cryptographic algorithm. This tool is a wrapper for the IISCryptoCli.exe tool.
-	
-	.DESCRIPTION
-		Set-IISCrypto - Configure your server to use a strong cryptographic algorithm. This tool is a wrapper for the IISCryptoCli.exe tool which is a free tool that gives administrators the ability to enable or disable protocols, ciphers, hashes and key exchange algorithms on Windows Server 2008, 2012, 2016 and 2019. It also lets you reorder SSL/TLS cipher suites offered by IIS, change advanced settings, implement Best Practices and import custom templates. The GUI available on the Nartac Software site can be used to create a custom template that can be imported with this tool and can be installed using the Install-IISCrypto command.
+    .SYNOPSIS
+        Configure your server to use a strong cryptographic algorithm. This tool is a wrapper for the IISCryptoCli.exe tool.
+    
+    .DESCRIPTION
+        Configure your server to use a strong cryptographic algorithm. This tool is a wrapper for the IISCryptoCli.exe tool which is a free tool that gives administrators the ability to enable or disable protocols, ciphers, hashes, and key exchange algorithms on Windows Server 2008, 2012, 2016, and 2019. It also lets you reorder SSL/TLS cipher suites offered by IIS, change advanced settings, implement Best Practices, and import custom templates. The GUI available on the Nartac Software site can be used to create a custom template that can be imported with this tool and can be installed using the Install-IISCrypto command.
 
-	.PARAMETER backup
-		Specify this switch to backup the current registry settings.
+    .PARAMETER backup
+        Specify a file to backup the registry to. Provide the filename and path for the backup.
 
-	.PARAMETER template
-		There are several predevined templates that can be used. The following are the templates that are available.
+    .PARAMETER template
+        Select a preconfigured template. The available templates are:
+        
+        - default: Restores the server to the default settings.
+        - best: Uses best practices for TLS, compatible with as many browsers as possible while disabling weak protocols and cipher suites.
+        - pci40: Makes your server PCI 4.0 compliant, disabling TLS 1.0 and 1.1 which may break client connections to your website. Ensure that RDP will continue to function as Windows 2008 R2 requires an update. See our FAQ for more information.
+        - strict: Uses the strictest settings possible, disabling TLS 1.0 and 1.1 and all non-forward secrecy cipher suites which may break client connections to your website. Ensure that RDP will continue to function as Windows 2008 R2 requires an update. See our FAQ for more information.
+        - fips140: Makes your server FIPS 140-2 compliant, similar to the Best Practices template but not as secure as Best Practices because some weaker cipher suites are enabled.
 
-		default		This template restores the server to the default settings.
+    .PARAMETER custom
+        Specify a custom template to use. Provide the filename and path of the custom template.
 
-		best		This template sets your server to use the best practices for TLS. It aims to be compatible with as many browsers as possible while disabling weak
-					protocols and cipher suites.
+    .PARAMETER reboot
+        Reboot the server after the settings are applied.
 
-		pci32		This template is used to make your server PCI 3.2 compliant. It will disable TLS 1.0 and 1.1 which may break client connections to your website. Please
-					make sure that RDP will continue to function as Windows 2008 R2 requires an update. See our FAQ for more information.
+    .EXAMPLE
+        Set-IISCrypto -backup "C:\backup\backup.reg" -template best -reboot
 
-		strict		This template sets your server to use the strictest settings possible. It will disable TLS 1.0 and 1.1 and all non forward secrecy cipher suites which
-					may break client connections to your website. Please make sure that RDP will continue to function as Windows 2008 R2 requires an update. See our FAQ for more information.
+        This example backs up the registry to a file named backup.reg, applies the best practices template, and reboots the server.
+    
+    .EXAMPLE
+        Set-IISCrypto -custom "C:\templates\MyCustomTemplate.ictpl" -reboot
 
-		fips140		This template makes your server FIPS 140-2 compliant. It is similar to the Best Practices template, however, it is not as secure as Best Practices
-					because some of the weaker cipher suites are enabled.
+        This example applies a custom template named MyCustomTemplate.ictpl and reboots the server.
 
-	.PARAMETER custom
-		This parameter allows you to specify a custom template to use. Please enter the filename and path of the template you wish to use.
+    .EXAMPLE
+        Set-IISCrypto -template pci40 -backup "C:\backup\backup.reg"
 
-	.PARAMETER filename
-		The filename and path of registry settings to backup.
+        This example backs up the registry to a file named backup.reg and applies the PCI 4.0 compliance template.
 
-	.PARAMETER reboot
-		This switch will reboot the server after the settings are applied.
+    .OUTPUTS
+        System.String
+    
+    .NOTES
+        Author: Luke Leigh
+        Website: https://scripts.lukeleigh.com/
+        LinkedIn: https://www.linkedin.com/in/lukeleigh/
+        GitHub: https://github.com/BanterBoy/
+        GitHubGist: https://gist.github.com/BanterBoy
+    
+    .INPUTS
+        backup - You can pipe objects to this parameter.
+        template - You can pipe objects to this parameter.
+        custom - You can pipe objects to this parameter.
+        reboot - You can pipe objects to this parameter.
 
-	.EXAMPLE
-		iiscryptocli /backup backup.reg /template "C:\temp\MyServers.ictpl" /reboot
+    .LINK
+        https://scripts.lukeleigh.com
+    #>
 
-		Here is an example that backs up the registry to a file named backup.reg, applies a custom template named MyServers.ictpl and reboots the server.
-	
-	.OUTPUTS
-		System.String. 
-	
-	.NOTES
-		Author:     Luke Leigh
-		Website:    https://scripts.lukeleigh.com/
-		LinkedIn:   https://www.linkedin.com/in/lukeleigh/
-		GitHub:     https://github.com/BanterBoy/
-		GitHubGist: https://gist.github.com/BanterBoy
-	
-	.INPUTS
-		backup - You can pipe objects to this parameter.
-		template - You can pipe objects to this parameter.
-		filename - You can pipe objects to this parameter.
-		reboot - You can pipe objects to this parameter.
-
-	.LINK
-		https://scripts.lukeleigh.com
-		Get-Date
-		Start-Process
-		Write-Output
-	#>
-
-	[CmdletBinding(DefaultParameterSetName = 'Default',
-		ConfirmImpact = 'Medium',
-		SupportsShouldProcess = $true)]
+	[CmdletBinding(DefaultParameterSetName = 'Default', SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
 	[alias("IISC")]
-
 	param (
-		[Parameter(Mandatory = $false,
-			Position = 0,
-			ValueFromPipeline = $true,
-			ValueFromPipelineByPropertyName = $true,
-			ParameterSetName = ('Default'),
-			HelpMessage = "This switch can be enabled to provide a backup of the current registry key.")]
+		[Parameter(Mandatory = $false, Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Specify a file to backup the registry to.")]
 		[string]
 		$backup,
-		[Parameter(Mandatory = $false,
-			Position = 0,
-			ValueFromPipeline = $true,
-			ValueFromPipelineByPropertyName = $true,
-			ParameterSetName = ('Template'),
-			HelpMessage = "This switch can be enabled to select a preconfigured template.")]
-		[ValidateSet ('best', 'pci32', 'strict', 'fips140', 'default')]
+
+		[Parameter(Mandatory = $false, Position = 1, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Select a preconfigured template.")]
+		[ValidateSet('best', 'pci40', 'strict', 'fips140', 'default')]
 		[string]
 		$template,
-		[Parameter(Mandatory = $false,
-			Position = 1,
-			ValueFromPipeline = $true,
-			ValueFromPipelineByPropertyName = $true,
-			ParameterSetName = ('Default', 'Template', 'Custom'),
-			HelpMessage = "Please enter a filename.")]
+
+		[Parameter(Mandatory = $false, Position = 2, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, HelpMessage = "Specify the file path and filename of the custom template.")]
 		[string]
-		$filename,
-		[Parameter(Mandatory = $false,
-			Position = 2,
-			ValueFromPipeline = $true,
-			ValueFromPipelineByPropertyName = $true,
-			ParameterSetName = ('Default', 'Template', 'Custom'),
-			HelpMessage = "This switch can be enabled to reboot the computer after the template has been applied.")]
-		[bool]
-		$reboot,
-		[Parameter(Mandatory = $false,
-			position = 0,
-			ValueFromPipeline = $true,
-			ValueFromPipelineByPropertyName = $true,
-			ParameterSetName = ('Custom'),
-			HelpMessage = "Please enter the file path and filename of the custom template.")]
-		[string]
-		$templateFile
+		$custom,
+
+		[Parameter(Mandatory = $false, Position = 3, HelpMessage = "Reboot the computer after the template has been applied.")]
+		[switch]
+		$reboot
 	)
-	begin {
-		
-	}
+
 	process {
+		$EnvPath = Get-EnvPath -Container Machine | Where-Object -FilterScript { $_ -Like '*IISCrypto*' }
+
+		if ($backup) {
+			if ($PSCmdlet.ShouldProcess("Backup", "Backing up registry to $backup")) {
+				& "$EnvPath\IISCryptoCli.exe" /backup $backup
+			}
+		}
+
+		if ($template) {
+			if ($PSCmdlet.ShouldProcess("Template", "Applying template $template")) {
+				& "$EnvPath\IISCryptoCli.exe" /template $template
+			}
+		}
+
 		if ($custom) {
-			$EnvPath = Get-EnvPath -Container Machine | Where-Object -FilterScript { $_ -Like '*IISCrypto*' }
-			if ($reboot) {
-				if ($PSCmdlet.ShouldProcess("$templateFile", "Applying custom templatefile and rebooting the computer.")) {
-					& $EnvPath\IISCryptoCli.exe /custom $templateFile /reboot
-				}
-			}
-			else {
-				if ($PSCmdlet.ShouldProcess("$templateFile", "Applying custom templatefile")) {
-					& $EnvPath\IISCryptoCli.exe /custom $templateFile
-				}	
+			if ($PSCmdlet.ShouldProcess("Custom Template", "Applying custom template $custom")) {
+				& "$EnvPath\IISCryptoCli.exe" /template $custom
 			}
 		}
-		elseif ($backup) {
-			$EnvPath = Get-EnvPath -Container Machine | Where-Object -FilterScript { $_ -Like '*IISCrypto*' }
-			if ($reboot) {
-				if ($PSCmdlet.ShouldProcess("$templateFile", "Performing backup of registry to $filename settings and applying template")) {
-					& $EnvPath\IISCryptoCli.exe /backup $filename /template $templateFile /reboot
-				}
-			}
-			else {
-				if ($PSCmdlet.ShouldProcess("$templateFile", "Performing backup of registry to $filename settings and applying template")) {
-					& $EnvPath\IISCryptoCli.exe /backup $filename /template $templateFile
-				}
+
+		if ($reboot.IsPresent) {
+			if ($PSCmdlet.ShouldProcess("Reboot", "Rebooting the computer")) {
+				& "$EnvPath\IISCryptoCli.exe" /reboot
 			}
 		}
-		else {
-			switch ($template) {
-				best {
-					$EnvPath = Get-EnvPath -Container Machine | Where-Object -FilterScript { $_ -Like '*IISCrypto*' }
-					if ($reboot) {
-						if ($PSCmdlet.ShouldProcess("$template", "Applying predefined template and rebooting the computer.")) {
-							& $EnvPath\IISCryptoCli.exe /template $template /reboot
-						}
-					}
-					else {
-						if ($PSCmdlet.ShouldProcess("$template", "Applying predefined template")) {
-							& $EnvPath\IISCryptoCli.exe /template $template
-						}	
-					}
-				}
-				pci32 {
-					$EnvPath = Get-EnvPath -Container Machine | Where-Object -FilterScript { $_ -Like '*IISCrypto*' }
-					if ($reboot) {
-						if ($PSCmdlet.ShouldProcess("$template", "Applying predefined template and rebooting the computer.")) {
-							& $EnvPath\IISCryptoCli.exe /template $template /reboot
-						}
-					}
-					else {
-						if ($PSCmdlet.ShouldProcess("$template", "Applying predefined template")) {
-							& $EnvPath\IISCryptoCli.exe /template $template
-						}	
-					}
-				}
-				strict {
-					$EnvPath = Get-EnvPath -Container Machine | Where-Object -FilterScript { $_ -Like '*IISCrypto*' }
-					if ($reboot) {
-						if ($PSCmdlet.ShouldProcess("$template", "Applying predefined template and rebooting the computer.")) {
-							& $EnvPath\IISCryptoCli.exe /template $template /reboot
-						}
-					}
-					else {
-						if ($PSCmdlet.ShouldProcess("$template", "Applying predefined template")) {
-							& $EnvPath\IISCryptoCli.exe /template $template
-						}	
-					}
-				}
-				fips140 {
-					$EnvPath = Get-EnvPath -Container Machine | Where-Object -FilterScript { $_ -Like '*IISCrypto*' }
-					if ($reboot) {
-						if ($PSCmdlet.ShouldProcess("$template", "Applying predefined template and rebooting the computer.")) {
-							& $EnvPath\IISCryptoCli.exe /template $template /reboot
-						}
-					}
-					else {
-						if ($PSCmdlet.ShouldProcess("$template", "Applying predefined template")) {
-							& $EnvPath\IISCryptoCli.exe /template $template
-						}	
-					}
-				}
-				default {
-					$EnvPath = Get-EnvPath -Container Machine | Where-Object -FilterScript { $_ -Like '*IISCrypto*' }
-					if ($reboot) {
-						if ($PSCmdlet.ShouldProcess("$template", "Applying predefined template and rebooting the computer.")) {
-							& $EnvPath\IISCryptoCli.exe /template $template /reboot
-						}
-					}
-					else {
-						if ($PSCmdlet.ShouldProcess("$template", "Applying predefined template")) {
-							& $EnvPath\IISCryptoCli.exe /template $template
-						}	
-					}
-				}
-			}
-		}
-	}
-	end {
-		
 	}
 }
